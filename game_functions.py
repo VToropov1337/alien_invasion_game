061e528cd520dc1455298abfa51cb4134d155ab4
Торопов Владимир <v.toropov@mac305rds.rambler.ramblermedia.com>
2019-11-05 18:14:00 +0300

    save

---
+++ game_functions.py
@@ -0,0 +1,62 @@
+import sys
+import pygame
+from bullet import Bullet
+
+def check_events(ai_settings, screen, ship, bullets):
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Реагирует на нажатие клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Создание новой пули и включение ее в группу bullets.
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+
+
+def update_screen(ai_settings, screen, ship, bullets):
+    screen.fill(ai_settings.bg_color)
+    # Все пули выводятся позади изображений корабля и пришельцев.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+    ship.blitme()
+
+
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули."""
+       # Обновление позиций пуль.
+    bullets.update()
+       # Удаление пуль, вышедших за край экрана.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум еще не достигнут."""
+   # Создание новой пули и включение ее в группу bullets.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+    
---
+++ settings.py
@@ -0,0 +1,13 @@
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 640
+        self.screen_height = 480
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 3.5
+        # Параметры пули
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
