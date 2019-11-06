a0957c9f80ef5247bf04627e780218e35cb742df
Торопов Владимир <v.toropov@mac305rds.rambler.ramblermedia.com>
2019-11-06 18:24:24 +0300

    save

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings():
     def __init__(self):
-        self.screen_width = 640
-        self.screen_height = 480
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 3.5
         # Параметры пули
@@ -10,4 +10,7 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
-
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
